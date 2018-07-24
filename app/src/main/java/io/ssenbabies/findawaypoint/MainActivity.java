64e52b039fc4bf2d8974347b22f50bdf4c3de76c
Devist <aboutdh89@naver.com>
2018-07-25 00:31:31 +0900

    (2018-07-25) add by Devist : Implement My Location

--- app/src/main/java/io/ssenbabies/findawaypoint/MainActivity.java
+++ app/src/main/java/io/ssenbabies/findawaypoint/MainActivity.java
@@ -1,13 +1,36 @@
 package io.ssenbabies.findawaypoint;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnAddRoom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setLayout();
+
+    }
+
+    private void setLayout(){
+        btnAddRoom = (Button) findViewById(R.id.btnAddRoom);
+
+        setListener();
+    }
+
+    private void setListener(){
+
+        btnAddRoom.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RoomActivity.class));
+            }
+        });
     }
 }
