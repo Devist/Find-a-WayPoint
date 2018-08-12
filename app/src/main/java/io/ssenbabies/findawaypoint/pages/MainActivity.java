45f4532c5c7cb3ffa3b64946af6897a1cfb8e86f
KimTaeJun <xowns4817@naver.com>
2018-08-13 00:03:18 +0900

    2018.8.13 위치권한 허용 다이얼로그

--- app/src/main/java/io/ssenbabies/findawaypoint/pages/MainActivity.java
+++ app/src/main/java/io/ssenbabies/findawaypoint/pages/MainActivity.java
@@ -1,6 +1,9 @@
 package io.ssenbabies.findawaypoint.pages;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -27,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //위치 권한 다이얼로그
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // 위치 정보 접근 요청
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+        }
 
         setLayout();
     }
