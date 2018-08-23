bd2b0b16005b2b5e75e369156bc211a89a0b0830
Devist <aboutdh89@naver.com>
2018-08-24 00:07:05 +0900

    (2018-08-24) add by Devist : 나의 출발위치 입력하기 페이지 구현 완료

--- app/src/main/java/io/ssenbabies/findawaypoint/views/MyLocationActivity.java
+++ app/src/main/java/io/ssenbabies/findawaypoint/views/MyLocationActivity.java
@@ -3,10 +3,7 @@ package io.ssenbabies.findawaypoint.views;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
@@ -18,14 +15,11 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
-
-import org.json.JSONObject;
 
 import io.ssenbabies.findawaypoint.R;
 import io.ssenbabies.findawaypoint.network.WaySocket;
 
-public class CreateActivity extends AppCompatActivity {
+public class MyLocationActivity extends AppCompatActivity {
 
     private ImageButton btnCancel;
     private Button btnShare;
@@ -35,9 +29,7 @@ public class CreateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create);
-
-        WaySocket.currentView = getCurrentFocus();
+        setContentView(R.layout.activity_mylocation);
         setLayout();
     }
 
@@ -61,55 +53,6 @@ public class CreateActivity extends AppCompatActivity {
             }
         });
 
-
-        btnShare.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(editAppointment.isEnabled()){
-                    editAppointment.setEnabled(false);
-                    btnShare.setText("약속하러 가기");
-                }else{
-                    startActivity(new Intent(getApplicationContext(),MyLocationActivity.class));
-                }
-
-            }
-        });
-
-        WaySocket.getInstance().setWaySocketListener(new WaySocket.WaySocketListener() {
-            @Override
-            public void onCreateResultReceived(View v, JSONObject result) {
-                hideKeyboard(getCurrentFocus());
-                Log.d("소켓 방 생성 결과 : ","결과받음");
-                Log.d("소켓",result.toString());
-                try{
-
-                    int status = result.getInt("status");
-                    if(status==WaySocket.SUCCESS){
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Snackbar.make(btnShare, "생성 완료! 공유하고 약속 장소를 정하러 가요", Snackbar.LENGTH_SHORT).show();
-                                btnShare.setVisibility(View.VISIBLE);
-                            }
-                        });
-
-                    }else if(status==WaySocket.SUCCESS){
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Snackbar.make(btnShare, "생성 실패! 서버 개발자가 잘못했습니다", Snackbar.LENGTH_SHORT).show();
-                            }
-                        });
-
-                    }
-
-                }catch(Exception e){
-                    Snackbar.make(btnShare, "통신에 실패했습니다.", Snackbar.LENGTH_SHORT).show();
-                    e.printStackTrace();
-                }
-            }
-        });
-
         editAppointment.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -119,7 +62,7 @@ public class CreateActivity extends AppCompatActivity {
                 //final int DRAWABLE_BOTTOM = 3;
 
                 if(event.getAction() == MotionEvent.ACTION_UP) {
-                    if(event.getRawX() >= (editAppointment.getRight() - editAppointment.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                    if(event.getX() >= (editAppointment.getRight() - editAppointment.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
                         Log.d("소켓","방 생성 시도");
                         WaySocket.getInstance().requestCreateRoom(editAppointment.getText().toString(),"");
 
@@ -127,6 +70,34 @@ public class CreateActivity extends AppCompatActivity {
                     }
                 }
                 return false;
+            }
+        });
+
+        editAppointment.addTextChangedListener(new TextWatcher() {
+            @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+            @Override public void afterTextChanged(Editable editable) { }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (editAppointment.getText().length()>2){
+                    btnShare.setVisibility(View.VISIBLE);
+                }else{
+                    btnShare.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editAppointment.isEnabled()){
+                    editAppointment.setEnabled(false);
+                    btnShare.setText("약속하러 가기");
+                    hideKeyboard(v);
+                }else{
+
+                }
+
             }
         });
 
@@ -170,6 +141,5 @@ public class CreateActivity extends AppCompatActivity {
     private void hideKeyboard(View view) {
         InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
-
     }
 }
